505afc9813c20fed3d28e0b61f8a3f3bc3723f60
TechDecryptor <145176897+TechDecryptor@users.noreply.github.com>
2023-10-13 15:25:52 +0000

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,20 @@
 use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::error::Error;
-use urlencoding::encode;
 
 #[no_mangle]
 pub fn tts(
     text: &str,
     lang: &str,
-    needs: HashMap<String, String>,
+    _needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
-    let mut url = match needs.get("requestPath") {
-        Some(url) => url.to_string(),
-        None => "lingva.pot-app.com".to_string(),
-    };
 
-    if !url.starts_with("http") {
-        url = format!("https://{}", url);
-    }
-
-    let plain_text = text.replace("/", "%2F");
-    let encode_text = encode(plain_text.as_str());
+    let url = "https://dict.youdao.com/dictvoice";
 
     let res = client
-        .get(format!("{url}/api/v1/audio/{lang}/{encode_text}"))
+        .get(url)
+        .query(&[("audio", text), ("le", lang)])
         .send()?
         .bytes()?;
 
@@ -37,8 +28,7 @@ mod tests {
     use super::*;
     #[test]
     fn try_request() {
-        let mut needs = HashMap::new();
-        needs.insert("requestPath".to_string(), "lingva.pot-app.com".to_string());
+        let needs = HashMap::new();
         let result = tts("你好", "zh", needs).unwrap();
         println!("{result}");
     }
